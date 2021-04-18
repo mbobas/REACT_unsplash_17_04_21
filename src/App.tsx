57d47f95781eb06992084b9c0bd704125d55e1f2
Michal <mbobas@gmail.com>
2021-04-18 17:11:04 +0200

    Absolute paths2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import "./App.css";
+import "App.css";
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
-import Home1 from './pages/home/Home1';
-import TestPage from './pages/test/TestPage';
-import ResultsPage from './pages/resultspage/ResultsPage';
+import Home1 from 'pages/home/Home1';
+import TestPage from 'pages/test/TestPage';
+import ResultsPage from 'pages/resultspage/ResultsPage';
 
 export default function App() {
     return (
--- src/components/autocomplete/RenderListAutocomplete.tsx
+++ src/components/autocomplete/RenderListAutocomplete.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
-import "./RenderListAutocomplete.css";
+import "components/autocomplete/RenderListAutocomplete";
 
 interface RenderListAutocompleteProps {
     resultCollection: any,
